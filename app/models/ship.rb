61f399f4c436964c5bb9b6321ad608e6a7d7afd3
erikn775 <erikn775@gmail.com>
2020-11-29 16:50:17 -0700

    commit tree 9ad3e086706462d3c79b88ee9b05554df5d5c1b3

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+    attr_accessor :name, :type, :booty
+
+    SHIPS = []
+
+    def initialize(args)
+        @name = args[:name]
+        @type = args[:type]
+        @booty = args[:booty]
+        @@all << self
+    end
+
+    def self.all
+        self
+    end
+
+    def self.clear
+        self.destroy_all
+    end
 end
